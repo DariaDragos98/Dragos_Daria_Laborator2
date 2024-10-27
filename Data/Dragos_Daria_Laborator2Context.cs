b7c1f368f27cabf8674268eaeb49e8c5f90aa152
Daria Dragos <Daria Dragos@DARIADRAGOS>
2024-10-27 20:52:50 +0200

    Add project files.

---
+++ Data/Dragos_Daria_Laborator2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Dragos_Daria_Laborator2.Models;
+
+namespace Dragos_Daria_Laborator2.Data
+{
+    public class Dragos_Daria_Laborator2Context : DbContext
+    {
+        public Dragos_Daria_Laborator2Context (DbContextOptions<Dragos_Daria_Laborator2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Dragos_Daria_Laborator2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Dragos_Daria_Laborator2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
